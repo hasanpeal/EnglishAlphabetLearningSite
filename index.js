0779d2115ad3a8fe74f2039b90a3e30a808ef348
Peal Hasan <pealh0320@gmail.com>
2024-06-05 21:15:17 -0400

    Completed keyboard interaction

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-$("img").click(function () {
+$("img").on("click", function () {
   var className = $(this).attr("class");
   var secondClass = className.slice(6, 12);
   $("." + secondClass)
-    .slideUp().slideDown();
+    .slideUp()
+    .slideDown();
   var slicedName = className.slice(11, 12);
   var audio = new Audio("./assets/sounds/" + slicedName + ".wav");
+  audio.play();
+});
+
+$(document).on("keydown", function (event) {
+  var letter = event.key.toUpperCase();
+  var targetClass = "image" + letter;
+  $("." + targetClass)
+    .slideUp()
+    .slideDown();
+  var audio = new Audio("./assets/sounds/" + letter + ".wav");
   audio.play();
 });
